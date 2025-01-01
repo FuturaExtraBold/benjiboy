1eba1cd359c6f1a4379bcbeb191021fd6c09a883
Ben Hays <ben@kajabi.com>
2025-01-01 14:49:09 -0800

    Added the dipshit image, and styling for the hero.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Hero.scss";
+import heroImage from "../../assets/images/dipshit.jpeg";
 
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="hero-image-container">
+        <img alt="Dipshit" className="hero-image" src={heroImage} />
+      </div>
       <h1>Hero</h1>
     </div>
   );
