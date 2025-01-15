c4996650eb5b67d7abfe2048ed9095a3c3f3d9a8
Ben Hays <ben@kajabi.com>
2025-01-15 12:21:55 -0800

    Added gh-pages package, updated sample--askew

--- src/components/Sample/Sample.jsx
+++ src/components/Sample/Sample.jsx
@@ -5,6 +5,14 @@ import { SampleModalContext } from "../../context/SampleModalContext";
 function Sample({ title, image, className }) {
   const { toggleModal } = useContext(SampleModalContext);
 
+  const randomRotation = Math.random() * 10 - 5;
+
+  const getStyles = () => {
+    if (className === "sample--askew") {
+      return { transform: `rotate(${randomRotation}deg)` };
+    } else return {};
+  };
+
   return (
     <div
       className={`sample ${className}`}
@@ -12,6 +20,7 @@ function Sample({ title, image, className }) {
         console.log("Sample component clicked");
         toggleModal();
       }}
+      style={getStyles()}
     >
       <img src={image} alt={title} className="sample__image" />
       <div className="sample__content">
